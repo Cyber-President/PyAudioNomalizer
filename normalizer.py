3578835fb9f6b9df120f81686f62e3890b19e7aa
shion <1901010642tx@cyber-u.ac.jp>
2021-04-28 22:28:00 +0900

    Function name modification.

--- normalizer.py
+++ normalizer.py
@@ -4,10 +4,10 @@ def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
-inport_sound = ""
-export_sound = "normalized_" + inport_sound
+import_sound = ""
+export_sound = "normalized_" + import_sound
 
-sound = AudioSegment.from_file(inport_sound, "mp3")
+sound = AudioSegment.from_file(import_sound, "mp3")
 normalized_sound = match_target_amplitude(sound, -20.0)
 normalized_sound.export(export_sound, format="mp3")
 
